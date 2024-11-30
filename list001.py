9061e95916e332f5f569ee04547702ba1b985f41
rinat <rinatyn777@gmail.com>
2024-11-30 22:29:12 +0300

    Добавление теории в list001.py

--- list001.py
+++ list001.py
@@ -12,3 +12,30 @@ pop
 remove
 reverse
 sort
+
+# Примеры использования методов
+# Часть примеров взята с сайта w3schools.com
+
+# Создание списка
+mylist = ["apple", "banana", "cherry"]
+mylist = ()
+
+# Получить длину списка
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist)) # 3
+
+# Списки могут иметь элементы разных типов
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 2, 3, 4, 5]
+list3 = [True, False, True]
+list4 = ["abc", 34, True, 40, "male"]
+
+# Уточнить действительно ли объект является списком (list)
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist)) # <class 'list'>
+
+# Для создание списка можно использовать метод list()
+thislist = list(("apple", "banana", "cherry"))
+print(thislist) # ["apple", "banana", "cherry"]
+
+
