bb0ef7f555773b711f84cec64520731cdb800c61
Rinatich <rinatyn777@gmail.com>
2025-07-05 15:53:07 +0300

    Изменение 2 в файле models.py

--- models.py
+++ models.py
@@ -3,3 +3,12 @@ from django.db import models
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+
+#---------------------------------------------
+
+from django.db import models
+
+class Memeber(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+
